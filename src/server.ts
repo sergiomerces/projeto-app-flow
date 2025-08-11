81ca3c8698aaba5fd6b32a0cf683c06f69676860
sergiomerces <sergio.merces@gmail.com>
2025-08-11 00:26:11 -0300

    criando server e porta

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello, World!');
+import * as http from 'http';
+
+//criando um servidor HTTP
+const server = http.createServer(
+    (request: http.IncomingMessage, response:http.ServerResponse) => {}
+);
+
+
+const port = process.env.PORT;
+
+//criando uma porta para o servidor
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
